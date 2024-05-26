2a85ea809d1a8ea563d4abd3ab2a2ec92920cb31
Devisowmya-547 <115868465+Devisowmya-547@users.noreply.github.com>
2024-05-26 16:06:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def predict():
         return render_template('home.html', prediction_text='The news is {}'.format(result))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
